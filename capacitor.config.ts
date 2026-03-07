acd00eef536b21f2fe171fe8a2d88123f10ae71a
PietroLearnAndMake <260706325+PietroLearnAndMake@users.noreply.github.com>
2026-03-06 23:05:15 -0500

    feat: Bloqueio de zoom e modo imersivo v1.8.5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.pietro.organizador.enem',
   appName: 'OrganiStuda',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: true,
+      style: 'DARK'
+    },
+    NavigationBar: {
+      visibility: 'hidden'
+    }
+  }
 };
 
 export default config;
